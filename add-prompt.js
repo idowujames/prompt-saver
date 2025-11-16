1b231bbc065194cc9d4180045e4e89408fefb152
james <jamesid2915@gmail.com>
2025-11-16 05:28:14 +0100

    First draft of saving prompts to localstorage

--- add-prompt.js
+++ add-prompt.js
@@ -1,1 +1,52 @@
 // TODO: Prevent the default form submission behavior
+
+
+
+const form = document.getElementById("add-form")
+const title = document.getElementById("prompt-title")
+const text = document.getElementById("prompt-text")
+const errorText = document.getElementById("error-text")
+
+const saveBtn = document.getElementById("save-prompt")
+
+
+form.addEventListener("submit", function(event){
+    // Prevent the default form submission behavior
+    event.preventDefault()
+
+    // Get the trimed values of the title and text
+    const promptTitle = title.value.trim()
+    const promptText = text.value.trim()
+
+    errorText.textContent = ""
+
+    // Quick validation of the input feilds to make sure they are not empty
+    if (promptTitle === "" || promptText === ""){
+        errorText.textContent = "Prompt title or text cannot be empty"
+        return false
+    } 
+
+
+    // Creating the new prompt
+    const newPrompt = {
+        "Title": promptTitle,
+        "Prompt": promptText
+    }
+
+    
+    let promptList = []
+    const prompts = localStorage.getItem("prompts")
+
+    if (prompts != null){
+        promptList = JSON.parse(prompts)
+    }
+
+
+    promptList.push(newPrompt)
+    console.log(promptList)
+
+    localStorage.setItem("prompts", JSON.stringify(promptList))
+
+})
+
+
